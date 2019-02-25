807531deffdd1d154781b418d812d82a0e762998
zgsxxzzyl <zgsxxzzyl@hotmail.com>
2019-02-25 16:45:27 +0800

    update

--- springcloud-config-client/src/main/java/com/SpringcloudConfigClientApplication.java
+++ springcloud-config-client/src/main/java/com/SpringcloudConfigClientApplication.java
@@ -3,11 +3,11 @@ package com;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@Controller
+@RestController
 public class SpringcloudConfigClientApplication {
 
     @Value("${foo}")
