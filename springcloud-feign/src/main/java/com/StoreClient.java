e91f32e1bdf202dd454132578311dfef28589281
zgsxxzzyl@hotmail.com <zgsxxzzyl@hotmail.com>
2020-11-23 11:09:46 +0800

    20201123

---
+++ springcloud-feign/src/main/java/com/StoreClient.java
@@ -0,0 +1,18 @@
+package com;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
+
+@FeignClient("stores")
+public interface StoreClient {
+    @RequestMapping(method = RequestMethod.GET, value = "/stores")
+    List<Store> getStores();
+
+    @RequestMapping(method = RequestMethod.POST, value = "/stores/{storeId}", consumes = "application/json")
+    Store update(@PathVariable("storeId") Long storeId, Store store);
+}
+
